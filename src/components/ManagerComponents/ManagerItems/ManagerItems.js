1df05999c65665f30c15388e44675f74357d4b52
madhu-famelyn <madhu@famelyn.com>
2025-10-06 13:13:16 +0530

    01:05, 10/6/2025 fixing error while deploy 2

--- src/components/ManagerComponents/ManagerItems/ManagerItems.js
+++ src/components/ManagerComponents/ManagerItems/ManagerItems.js
@@ -1,9 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import "./ManagerItems.css";
-
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./ManagerItems.css";
